a8e11c906e9608167935b8c30af73e978af59528
Vasil Nikolov <vaskonikolov2003@gmail.com>
2024-01-09 00:08:52 +0200

    goofin around

---
+++ examples/rp/src/bin/radio/main.rs
@@ -0,0 +1,131 @@
+#![no_std]
+#![no_main]
+
+use core::cell::RefCell;
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_rp::{gpio, PeripheralRef};
+use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
+use embassy_sync::channel::Channel;
+use embassy_sync::mutex::Mutex;
+use embassy_time::Duration;
+use embassy_time::{Instant, Ticker, Timer};
+use gpio::{AnyPin, Level, Output};
+use serde::{Deserialize, Serialize};
+use serde_json_core;
+use {defmt_rtt as _, panic_probe as _};
+
+type ErrType = ();
+type LedType = Mutex<ThreadModeRawMutex, Option<PeripheralRef<'static, Output<'static, AnyPin>>>>;
+static LED: LedType = Mutex::new(None);
+// size of queue for RF chip in bytes
+const RF_CHANNEL_SIZE: usize = 256;
+
+type ChannelType = Mutex<ThreadModeRawMutex, RefCell<Channel<ThreadModeRawMutex, u8, RF_CHANNEL_SIZE>>>;
+
+static RF_CHANNEL: ChannelType = Mutex::new(RefCell::new(Channel::new()));
+
+#[derive(Serialize, Deserialize)]
+enum Command {
+    Config(Config),
+    TransmitByte(u8),
+}
+
+#[derive(Serialize, Deserialize)]
+struct Config {
+    /// Transmit freqency, Hz
+    tx_freq: u64,
+    /// Receive freqency, Hz
+    rx_freq: u64,
+}
+
+impl Config {
+    fn default() -> Self {
+        Config { tx_freq: 1, rx_freq: 1 }
+    }
+}
+
+impl Command {
+    pub fn from_bytes(bytes: &[u8]) -> Result<Command, ErrType> {
+        if bytes.len() == 0 {
+            return Err(());
+        }
+        match serde_json_core::from_slice::<Command>(bytes) {
+            Ok((cmd, _)) => Ok(cmd),
+            Err(_) => Err(()),
+        }
+    }
+}
+
+trait CommandInput {
+    async fn get_command(&mut self) -> Command;
+}
+
+/// dummy Input source that sends random data every second
+struct RandomInput;
+
+impl CommandInput for RandomInput {
+    async fn get_command(&mut self) -> Command {
+        Timer::after_secs(1).await;
+        let time_since_start = Instant::now().as_micros();
+        if time_since_start % 10 == 0 {
+            return Command::Config(Config {
+                tx_freq: time_since_start,
+                rx_freq: u64::MAX - time_since_start,
+            });
+        } else {
+            return Command::TransmitByte(5 as u8);
+        }
+    }
+}
+
+#[embassy_executor::task]
+async fn fast_blink(led: &'static LedType, delay: Duration) {
+    let mut ticker = Ticker::every(delay);
+    let mut led_unlocked = led.lock().await;
+    // take for the whole duration of the fast ticking
+    if let Some(pin_ref) = led_unlocked.as_mut() {
+        for _ in 0..50 {
+            (*pin_ref).toggle();
+            ticker.next().await
+        }
+    }
+}
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    let p = embassy_rp::init(Default::default());
+
+    // set the content of the global LED reference to the real LED pin
+    let led = Output::new(AnyPin::from(p.PIN_25), Level::High);
+    {
+        *(LED.lock().await) = Some(PeripheralRef::new(led));
+    }
+    unwrap!(spawner.spawn(working_indicator(&LED, Duration::from_millis(500))));
+    let mut no_input_stream = RandomInput {};
+
+    loop {
+        match no_input_stream.get_command().await {
+            // Command::Config(_c) => fast_blink(&LED, Duration::from_millis(50)).await,
+            Command::Config(_c) => match spawner.spawn(fast_blink(&LED, Duration::from_millis(50))) {
+                Ok(_) => (),
+                Err(_) => (),
+            },
+            Command::TransmitByte(_b) => Timer::after_nanos(2).await,
+        }
+    }
+}
+
+#[embassy_executor::task]
+async fn working_indicator(led: &'static LedType, delay: Duration) {
+    let mut ticker = Ticker::every(delay);
+    loop {
+        {
+            let mut led_unlocked = led.lock().await;
+            if let Some(pin_ref) = led_unlocked.as_mut() {
+                pin_ref.toggle();
+            }
+        }
+        ticker.next().await
+    }
+}
